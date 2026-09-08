8eafcf853aa7719417268b3001df866f33f9fc01
Stanley Bathram Sunday <stanleysunday554@gmail.com>
2026-09-08 18:43:53 +0100

    update
    added playlist section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ArtistSection from '../component/artist-section'
-
+import PlayList from '../component/playlist-section'
 
 
 const App = () => {
   return (
     <main>
       <ArtistSection />
+      <PlayList />
     </main>
   )
 }
