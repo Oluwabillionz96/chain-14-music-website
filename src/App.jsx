ba0cf16dbc002e9e0a911e5f866c6771c7332069
joanneutuk05-svg <joanneutuk05@gmail.com>
2026-09-08 18:38:04 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ArtistSection from '../component/artist-section'
-
-
+import RecentlyPlayedSection from '../component/recently-played-section'
+// impor
 
 const App = () => {
   return (
     <main>
       <ArtistSection />
+      <RecentlyPlayedSection />
     </main>
   )
 }
